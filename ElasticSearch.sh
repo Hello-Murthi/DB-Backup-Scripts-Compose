19e16b658673d21a01ac3e789635f61bab466ce9
Murthi-DN <hello.murthi@gmail.com>
2022-04-28 05:16:29 +0530

    d

--- ElasticSearch.sh
+++ ElasticSearch.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+TODAY=`date +"%d%b%Y"`
+DB_BACKUP_PATH='/backup/elasticsearch'
+mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+LOGFILE=${DB_BACKUP_PATH}/logs.txt
+
 declare -a arr=("userevents" "botevents")
 touch event.txt
 for i in "${arr[@]}"
   do
     ts=$(date +%d-%m-%Y_%H-%M-%S)
-    elasticdump --input=http://elastic.esstack:9200/${i} --output=${i}_${ts}.json --type=data --searchBody="{\"query\": {\"range\": {\"@timestamp\": {\"gte\": \"now-1d/d\", \"lt\": \"now/d\"}}}}" --limit 1000
-    #aws s3 cp ${i}_${ts}.json s3://opteon-data-integration/
-    echo "${i}_${ts}" >> event.txt
-    #rm ${i}_${ts}.json
+    echo "Backup started for ${i} index at $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
+    elasticdump --input=http://localhost:9200/${i} --type=data --searchBody="{\"query\": {\"range\": {\"@timestamp\": {\"gte\": \"now-1d/d\", \"lt\": \"now/d\"}}}}" --limit 1000 --output=$ | gzip > ${DB_BACKUP_PATH}/${TODAY}/${i}_${ts}.json.gz
+    echo "File Path: ${DB_BACKUP_PATH}/${TODAY}/${i}_${ts}.json.gz" >> "$LOGFILE"
+    echo "Backup finished for ${i} index at $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
+    #aws s3 cp ${DB_BACKUP_PATH}/${TODAY}/${i}_${ts}.json.gz s3://opteon-data-integration/
+    echo "*****************" >> "$LOGFILE"
+    #rm ${DB_BACKUP_PATH}/${TODAY}/${i}_${ts}.json.gz
   done
 sleep 60
 
 
-
-#ZIP
