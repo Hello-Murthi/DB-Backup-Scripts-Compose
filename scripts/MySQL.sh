3f3bfdbdceeb771554257082ce14fb6c942dda86
Murthi-DN <hello.murthi@gmail.com>
2022-05-04 01:09:23 +0530

    d

--- scripts/MySQL.sh
+++ scripts/MySQL.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
+
 TODAY=`date +"%d%b%Y"`
-DB_BACKUP_PATH='/dbbackup/mysql'
-mkdir -p ${DB_BACKUP_PATH}
-mkdir ${TODAY}
-LOGFILE=${DB_BACKUP_PATH}/backup_logs.txt
+BACKUP_PATH='/dbbackup/mysql'
+mkdir -p ${BACKUP_PATH}
+mkdir MySQL_${TODAY}
+LOGFILE=${BACKUP_PATH}/logs.txt
 
 DATABASE_NAMES='ym'
 
 for DB_NAME in ${DATABASE_NAMES}
 do
-    echo "MySql dump started for ${DB_NAME} at $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
-    mysqldump --column-statistics=0 --host=mysql --port=3306 --user=${DBUSER} --password=${DBPASSWD} --default-character-set=utf8  ${DB_NAME} > ${DB_NAME}.sql
-    mv ${DB_NAME}.sql ${TODAY}
-    echo "MySql dump finished for ${DB_NAME} at $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
-
+    echo "Backup started for ${DB_NAME} at $(date +'%d-%m-%Y %H:%M:%S')" >> ${LOGFILE}
+    mysqldump --host=mysql --port=3306 --user=${USER} --password=${PASSWORD} --default-character-set=utf8 --column-statistics=0 ${DB_NAME} > ${DB_NAME}.sql
+    mv ${DB_NAME}.sql MySQL_${TODAY}
+    echo "Backup finished for ${DB_NAME} at $(date +'%d-%m-%Y %H:%M:%S')" >> ${LOGFILE}
+    echo " " >> ${LOGFILE}
 done
 
-tar -zcvf /${DB_BACKUP_PATH}/mysqlbackup_${TODAY}.tar.gz ${TODAY}
-rm -rf ${TODAY}
+tar -zcvf /${BACKUP_PATH}/MySQL_${TODAY}.tar.gz MySQL_${TODAY}
+rm -rf MySQL_${TODAY}
+find ${BACKUP_PATH} -type f -mtime +3 -name '*.gz' -delete
 
-find ${DB_BACKUP_PATH} -type f -mtime +3 -name '*.gz' -delete
-
-echo "*****************" >> "$LOGFILE"
+echo "*****************" >> ${LOGFILE}
--- scripts/Postgres.sh
+++ scripts/Postgres.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+
 TODAY=`date +"%d%b%Y"`
-DB_BACKUP_PATH='/dbbackup/postgress'
-mkdir -p ${DB_BACKUP_PATH}
-mkdir ${TODAY}
-LOGFILE=${DB_BACKUP_PATH}/backup_logs.txt
+BACKUP_PATH='/dbbackup/postgres'
+mkdir -p ${BACKUP_PATH}
+mkdir Postgres_${TODAY}
+LOGFILE=${BACKUP_PATH}/logs.txt
 
-HOSTNAME='postgres'
-USERNAME='postgres'
-export PGPASSWORD="$DBPASSWD"
+HOST='postgres'
+USER='postgres'
+export PGPASSWORD=${PASSWORD}
 
-DATABASE='contacts postgres template0 template1'
+DATABASE_NAMES='contacts postgres template1'
 
-for i in ${DATABASE}
+for DB_NAME in ${DATABASE_NAMES}
 do
-    echo "Postgres dump started for ${i} at $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
-    pg_dump -h $HOSTNAME -p 5432 -U $USERNAME -d ${i} > ${i}.backup
-    mv ${i}.backup ${TODAY}
-    echo "Postgres dump finished for ${i} at $(date +'%d-%m-%Y %H:%M:%S')" >> "$LOGFILE"
+    echo "Backup started for ${DB_NAME} at $(date +'%d-%m-%Y %H:%M:%S')" >> ${LOGFILE}
+    pg_dump -h $HOST -p 5432 -U $USER -d ${DB_NAME} > ${DB_NAME}.backup
+
+    if [ "$?" -eq 0 ]; then
+        mv ${DB_NAME}.backup Postgres_${TODAY}
+    else
+        echo "ERROR: ${DB_NAME}.backup has failed, please check..." >> ${LOGFILE}
+    fi
+    
+    echo "Backup finished for ${DB_NAME} at $(date +'%d-%m-%Y %H:%M:%S')" >> ${LOGFILE}
+    echo " " >> ${LOGFILE}
 done
 
-tar -zcvf /${DB_BACKUP_PATH}/postgresbackup_${TODAY}.tar.gz ${TODAY}
-rm -rf ${TODAY}
-
-find ${DB_BACKUP_PATH} -type f -mtime +3 -name '*.gz' -delete
-
+tar -zcvf /${BACKUP_PATH}/Postgres_${TODAY}.tar.gz Postgres_${TODAY}
+rm -rf Postgres_${TODAY}
+find ${BACKUP_PATH} -type f -mtime +3 -name '*.gz' -delete
 unset PGPASSWORD
 
-echo "*****************" >> "$LOGFILE"
+echo "*****************" >> ${LOGFILE}
 
 
